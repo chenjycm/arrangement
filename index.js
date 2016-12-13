22b5584aca093b4289f47a5526a03739d902b429
chenjycm <chenjycm@163.com>
2016-12-14 00:28:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function(){
+	console.log('ok!');
+	
+
+
+$('#button_range1').bind('click',function(){
+	var data_get=$('#textarea1').val();
+	var num_each=$('#input_num').val();
+	console.log('读取数据：'+data_get);
+	console.log('设置元素个数：'+num_each);
+});
+
+
+
+});
